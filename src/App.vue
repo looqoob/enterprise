e40ca8435950bce813c60c21c58b9564537a77a3
looqoob <3350417264@qq.com>
2023-02-13 17:16:01 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <nav>
+      <h1 class="hover">拉拉咯噜噜</h1>
+      <h1 class="h2 btnClick btnClickWave btnClickLeft hover">拉拉咯噜噜</h1>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
@@ -16,10 +18,12 @@
   text-align: center;
   color: #2c3e50;
 }
-
+h1 {
+  display: inline-block;
+  overflow: hidden;
+}
 nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
